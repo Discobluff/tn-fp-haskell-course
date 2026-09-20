fe8985fe1d7e65d8cef9a4aee2bed0716cd58291
Hugo Dartoy <hugo.dartoy@telecomnancy.eu>
2026-09-20 09:17:18 +0000

    TP3 parsing + tests

--- tps/TP3.hs
+++ tps/TP3.hs
@@ -10,10 +10,24 @@ import Debug.Trace
 import GHC.Generics
 import Generic.Random
 import Test.QuickCheck
+import Network.HTTP.Req (Url)
+import Data.List.Split
+import Data.List
 
 main :: IO ()
 main = do
   putStrLn "TP3 is running"
+  putStrLn "--------------"
+  putStrLn "Passing tests:"
+  putStrLn $ "Testing checkURLParsing on 'http://www.google.fr/': " ++ show (checkURLParsing "http://www.google.fr/")
+  putStrLn $ "Testing checkURLParsing on 'https://github.com/dmjio/miso/pulls': " ++ show (checkURLParsing "https://github.com/dmjio/miso/pulls")
+  putStrLn $ "Testing checkURLParsing on 'http://reddit.com/r/haskell': " ++ show (checkURLParsing "http://reddit.com/r/haskell")
+  putStrLn "--------------"
+  putStrLn "Failing tests:"
+  putStrLn $ "Testing checkURLParsing on 'htp://www.google.fr/': " ++ show (checkURLParsing "htp://www.google.fr/")
+  putStrLn $ "Testing checkURLParsing on 'https://github.co/dmjio/miso/pulls': " ++ show (checkURLParsing "https://github.co/dmjio/miso/pulls")
+  putStrLn $ "Testing checkURLParsing on 'http://redditcom/r/haskell': " ++ show (checkURLParsing "http://redditcom/r/haskell")
+
 
 -- 1/ Define a type representing URLs you can enter in a browser address bar,
 --    i.e. strings of the form:
@@ -38,3 +52,65 @@ main = do
 --
 -- Use https://hoogle.haskell.org/ to find the functions you need, for example splitOn:
 -- https://hackage.haskell.org/package/split-0.2.5/docs/Data-List-Split.html#v:splitOn
+
+data Protocol = HTTP | HTTPS
+data Extension = FR | COM
+
+instance Show Protocol where
+  show HTTP = "http"
+  show HTTPS = "https"
+
+instance Show Extension where
+  show FR = "fr"
+  show COM = "com"
+
+data URL = DefURL {
+  protocol :: Protocol,
+  subdomains :: [String],
+  domain :: String,
+  extension :: Extension,
+  path :: [String]
+}
+
+instance Show URL where
+  show (DefURL protocol subdomains domain extension path) = show protocol ++ "://" ++ formatSubdomains subdomains ++ domain ++ "." ++ show extension ++ "/" ++ intercalate "/" path
+
+
+formatSubdomains :: [String] -> String
+formatSubdomains subdomains = case subdomains of 
+  [] -> ""
+  l -> intercalate "." l ++ "."
+
+parseURL :: String -> Either String URL
+parseURL url = case (splitOn "://" url) of
+  [protocol, rest] -> case parseProtocol protocol of
+    Right p -> parseRightURL rest p
+    Left m -> Left m
+  _ -> Left "Error: bad format for URL"
+
+parseProtocol :: String -> Either String Protocol
+parseProtocol "http" = Right HTTP
+parseProtocol "https" = Right HTTPS
+parseProtocol _ = Left "Error: bad format for protocol"
+
+parseExtension :: String -> Either String Extension
+parseExtension "fr" = Right FR
+parseExtension "com" = Right COM
+parseExtension _ = Left "Error: bad format for extension"
+
+parseRightURL :: String -> Protocol -> Either String URL
+parseRightURL url protocol = case (splitOn "/" url) of
+  (domains : path) -> parseDomains domains protocol path
+  _ -> Left "Error: bad format for URL"
+
+parseDomains :: String -> Protocol -> [String] -> Either String URL
+parseDomains domains protocol path = case reverse (splitOn "." domains) of
+  (extension : domain : subdomainsRev) -> case parseExtension extension of
+    Right e -> Right (DefURL { protocol = protocol, subdomains = reverse subdomainsRev, domain = domain, extension = e, path = path })
+    Left m -> Left m
+  _ -> Left "Error: bad format for URL"
+
+checkURLParsing :: String -> Bool
+checkURLParsing url = case parseURL url of
+  Right urlParsed -> url == show urlParsed
+  Left _ -> False
