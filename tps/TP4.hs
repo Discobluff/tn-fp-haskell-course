4644473083f215643861c6b711cae5f7a05ad5ec
Hugo Dartoy <hugo.dartoy@telecomnancy.eu>
2026-09-23 08:55:00 +0000

    add quicktest

--- tps/TP4.hs
+++ tps/TP4.hs
@@ -11,6 +11,7 @@ import GHC.Generics
 import Generic.Random
 import System.Process
 import Test.QuickCheck
+import Test.QuickCheck.Monadic
 import Data.Char (digitToInt)
 
 -- The goal of this TP is to implement an evaluator for arithmetic expressions.
@@ -53,14 +54,68 @@ main = do
   let t4 = "1+2-3+4-5"
   let t5 = "12-13+14"
   let t6 = "1-2-3-4-5-6+7"
+  let t7 = "-5+5"
+  let t8 = "--5"
+  let t9 = "1--5"
+  let t10 = "-8+8"
+  let t11 = "--7+5"
   putStrLn (showTest t1)
   putStrLn (showTest t2)
   putStrLn (showTest t3)
   putStrLn (showTest t4)
   putStrLn (showTest t5)
   putStrLn (showTest t6)
+  putStrLn (showTest t7)
+  putStrLn (showTest t8)
+  putStrLn (showTest t9)
+  putStrLn (showTest t10)
+  putStrLn (showTest t11)
+  putStrLn "--------------"
+  putStrLn "QUICKCHECK TESTS"
+  putStrLn "--------------"
+  quickCheck prop_eval_matches_python
+
 
 data Expr = Constant Int | Add Expr Expr | Neg Expr
+
+newtype ExprString = ExprString { getExprString :: String }
+
+instance Show ExprString where
+  show = getExprString
+
+instance Arbitrary ExprString where
+  arbitrary = ExprString <$> sized genExprString
+    where
+      genDigits :: Gen String
+      genDigits = do
+        n <- choose (0, 1000 :: Int)
+        return (show n)
+
+      genExprString :: Int -> Gen String
+      genExprString 0 = genDigits
+      genExprString n = do
+        k <- choose (1, max 1 (n `div` 2))
+        parts <- vectorOf k genDigits
+        ops <- vectorOf (k - 1) $ do
+          op <- elements ['+', '-']
+          unaryNeg <- arbitrary
+          return (op, unaryNeg)
+        return (build parts ops)
+
+      build :: [String] -> [(Char, Bool)] -> String
+      build [] _ = ""
+      build (p:ps) ops = p ++ concat (zipWith combine ops ps)
+        where
+          combine (op, un) q = op : (if un then '-' : q else q)
+
+prop_eval_matches_python :: ExprString -> Property
+prop_eval_matches_python s = monadicIO $ do
+  let expr = parse s.getExprString
+  pyResult <- run (pyEval s.getExprString)
+  let expected = read pyResult
+  let actual = eval expr
+  assert (actual == expected)
+
 
 showTest :: String -> String
 showTest test = "Parsing '" ++ test ++ "' gives: '" ++ show (parse test) ++ "' and is evaluated to: '" ++ show (eval (parse test)) ++ "'"
@@ -80,17 +135,18 @@ instance Show Expr where
 
 
 parse :: String -> Expr
-parse s = parseAux s 0 False
+parse s = parseAux s 0 False False
 
-parseAux :: String -> Int -> Bool -> Expr
-parseAux string current neg = case string of
+parseAux :: String -> Int -> Bool -> Bool ->Expr
+parseAux string current neg hasInt = case string of
   [] | neg -> Neg (Constant current)
   [] -> Constant current
-  c : q | isDigit c -> parseAux q (current*10 + digitToInt c) neg
-  '+' : q | neg -> Add (Neg (Constant current)) e where e = parseAux q 0 False
-  '+' : q -> Add (Constant current) e where e = parseAux q 0 False
-  '-' : q | neg -> Add (Neg (Constant current)) e where e = parseAux q 0 True
-  '-' : q -> Add (Constant current) e where e = parseAux q 0 True
+  c : q | isDigit c -> parseAux q (current*10 + digitToInt c) neg True
+  '+' : q | neg -> Add (Neg (Constant current)) e where e = parseAux q 0 False False
+  '+' : q -> Add (Constant current) e where e = parseAux q 0 False False
+  '-' : q | not hasInt -> e where e = parseAux q 0 (not neg) False
+  '-' : q | neg -> Add (Neg (Constant current)) e where e = parseAux q 0 True False
+  '-' : q -> Add (Constant current) e where e = parseAux q 0 True False
   _ -> Constant 0
 -- 12-13+14 Add(12, Add(Neg(13), 14))
 -- 1-2+3+4 Add(1, Add(Neg(2), Add))
